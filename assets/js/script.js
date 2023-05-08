ffdddc7d347c3b8cd064e33031f4cf1f9fd93f5b
Robson Oliveira Magalhães <robsonom92@hotmail.com>
2023-05-08 16:34:26 -0300

    Controle de vedas

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,46 +3,50 @@ var productList = [];
 function adicionarProduto() {
     var nome = document.getElementById("nome").value;
     var preco = document.getElementById("preco").value;
-    var estoque = document.getElementById("estoque").value;
+    var quantidade = document.getElementById("quantidade").value;
+    var produtos = document.getElementById("produtos").value;
 
-    var produto = { nome: nome, preco: preco, estoque: estoque };
+    var produto = { nome: nome, preco: preco, quantidade: quantidade, produtos : produtos};
     productList.push(produto);
 
     var novoProduto = document.createElement("li");
     var id = "produto" + (productList.length - 1);
-
+    
     novoProduto.innerHTML = nome + "<br/> - Preço: R$" + preco + 
-    " - Estoque: "+"<span id='"+id+"controlEstoque'>" + estoque + 
-    "</span> <button onclick='removerUnidade(" + (productList.length - 1) + ")'>Remover</button> <button onclick='adicionarUnidade(" + (productList.length - 1) + ")'>Adicionar</button><hr/>";
-
+    " - quantidade: "+"<span id='"+id+"controlquantidade'>" + quantidade + "<br/>Produto: " + produtos +
+    "</span> <button onclick='removerUnidade(" + (productList.length - 1) + ")'>Remover</button>";
+    
     document.getElementById("listaProdutos").appendChild(novoProduto);
+    
+}
 
-    if (estoque == 0) {
-        document.getElementById("esgotado").checked = true;
-    }
+function gerarListaProdutos() {
+    var produtos = document.getElementById("listaProdutos").innerHTML;
+    var file = new Blob([produtos], {type: "text/plain"});
+    var a = document.createElement("a");
+    var url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = "listaProdutos.html";
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function() {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);  
+    }, 0);
 }
 
 function removerUnidade(index) {
     var produto = productList[index];
     var estoqueSpan = document.getElementById("produto" + index + "controlEstoque");
-    var estoqueAtual = parseInt(estoqueSpan.textContent);
-    if (estoqueAtual > 0) {
-        var novoEstoque = estoqueAtual - 1;
-        estoqueSpan.textContent = novoEstoque;
-        produto.estoque = novoEstoque;
-        if (novoEstoque == 0) {
-            document.getElementById("produto" + index + "-esgotado").checked = true;
-        }
-    }
+    
 }
 
-function adicionarUnidade(index) {
-    var produto = productList[index];
-    var estoqueSpan = document.getElementById("produto" + index + "controlEstoque");
-    var estoqueAtual = parseInt(estoqueSpan.textContent);
-    if (estoqueSpan != 0){
-        var novoEstoque = estoqueAtual + 1;
-        estoqueSpan.textContent = novoEstoque;
-        produto.estoque = novoEstoque;
-    }
+function downloadFile(text, filename) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
 }
